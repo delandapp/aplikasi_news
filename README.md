925f22517118eee3fb0db8237b5921aec00aa39d
delandapp <alihandronikolas@gmail.com>
2024-05-14 15:00:47 +0700

    Setup App

---
+++ README.md
@@ -0,0 +1,3 @@
+# dicoding_news_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dicoding_news_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dicoding_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
